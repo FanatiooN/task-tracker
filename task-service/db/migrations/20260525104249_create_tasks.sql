fb7e2a962c210246d2bfd3e7a2fa18086a810752
fanatioon <nazirov.f.f@gmail.com>
2026-05-25 16:10:57 +0300

    fix: add user_id column in tasks table

--- task-service/db/migrations/20260525104249_create_tasks.sql
+++ task-service/db/migrations/20260525104249_create_tasks.sql
@@ -3,6 +3,7 @@ CREATE TYPE task_status AS ENUM ('in_progress', 'done', 'cancelled');
 
 CREATE TABLE tasks (
    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+   user_id UUID NOT NULL,
    title TEXT NOT NULL,
    description TEXT,
    status task_status NOT NULL,
