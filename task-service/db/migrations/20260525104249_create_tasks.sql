86d47d14339e5d1a13814d2678a3fc1ae28124af
fanatioon <nazirov.f.f@gmail.com>
2026-05-25 14:58:33 +0300

    feat: create tasks migration

---
+++ task-service/db/migrations/20260525104249_create_tasks.sql
@@ -0,0 +1,15 @@
+-- +goose Up
+CREATE TYPE task_status AS ENUM ('in_progress', 'done', 'cancelled');
+
+CREATE TABLE tasks (
+   id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+   title TEXT NOT NULL,
+   description TEXT,
+   status task_status NOT NULL,
+   created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+   updated_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+   deleted_at TIMESTAMPTZ
+);
+-- +goose Down
+DROP TABLE tasks;
+DROP TYPE task_status;
