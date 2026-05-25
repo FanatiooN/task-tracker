e73dc4462ca04b8f67ad3bee822c66253694bcba
fanatioon <nazirov.f.f@gmail.com>
2026-05-25 14:58:27 +0300

    feat: create users migration

---
+++ user-service/db/migrations/20260525104018_create_users.sql
@@ -0,0 +1,11 @@
+-- +goose Up
+CREATE TABLE users (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    name TEXT NOT NULL,
+    email TEXT NOT NULL UNIQUE,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+    deleted_at TIMESTAMPTZ
+);
+-- +goose Down
+DROP TABLE users;
