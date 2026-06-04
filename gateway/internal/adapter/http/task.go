283a5c2d021ba24935fbcb01326a3d7a8a1b60e1
fanatioon <nazirov.f.f@gmail.com>
2026-06-04 17:48:21 +0300

    feat(gateway): add UpdateTask handler

--- gateway/internal/adapter/http/task.go
+++ gateway/internal/adapter/http/task.go
@@ -56,3 +56,46 @@ func (h *TaskHandler) GetTask(w http.ResponseWriter, r *http.Request) {
 
 	writeJSON(w, http.StatusOK, response.Task)
 }
+
+func (h *TaskHandler) UpdateTask(w http.ResponseWriter, r *http.Request) {
+	id := r.PathValue("id")
+	if id == "" {
+		writeError(w, http.StatusBadRequest, "invalid request body")
+		return
+	}
+
+	var req struct {
+		Title       *string `json:"title"`
+		Description *string `json:"description"`
+		TaskStatus  *string `json:"taskStatus"`
+	}
+
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		writeError(w, http.StatusBadRequest, "invalid request body")
+		return
+	}
+
+	var status *taskpb.TaskStatus
+	if req.TaskStatus != nil {
+		value := taskpb.TaskStatus_value[*req.TaskStatus]
+		if value == 0 {
+			writeError(w, http.StatusBadRequest, "invalid task status")
+			return
+		}
+		v := taskpb.TaskStatus(value)
+		status = &v
+	}
+
+	response, err := h.client.UpdateTask(r.Context(), &taskpb.UpdateTaskRequest{
+		Id:          id,
+		Title:       req.Title,
+		Description: req.Description,
+		Status:      status,
+	})
+	if err != nil {
+		//writeGRPCError(w, err) // TODO
+		return
+	}
+
+	writeJSON(w, http.StatusOK, response.Task)
+}
