d9a004928bb083458cd1f5f04b2790222bb960d4
fanatioon <nazirov.f.f@gmail.com>
2026-06-06 15:47:53 +0300

    feat(gateway): add insecure credentials to grpc/task.go, grpc/user.go

--- gateway/internal/adapter/grpc/task.go
+++ gateway/internal/adapter/grpc/task.go
@@ -4,10 +4,11 @@ import (
 	taskpb "task-tracker/gen/proto/task"
 
 	grpclib "google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 func NewTaskClient(addr string) (taskpb.TaskServiceClient, error) {
-	conn, err := grpclib.NewClient(addr)
+	conn, err := grpclib.NewClient(addr, grpclib.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
 		return nil, err
 	}
--- gateway/internal/adapter/grpc/user.go
+++ gateway/internal/adapter/grpc/user.go
@@ -4,10 +4,11 @@ import (
 	userpb "task-tracker/gen/proto/user"
 
 	grpclib "google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 func NewUserClient(addr string) (userpb.UserServiceClient, error) {
-	conn, err := grpclib.NewClient(addr)
+	conn, err := grpclib.NewClient(addr, grpclib.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
 		return nil, err
 	}
