6457f60a6ea7af595713205f7694d36bbb166956
fanatioon <nazirov.f.f@gmail.com>
2026-06-06 15:47:13 +0300

    feat(gateway): add main

---
+++ gateway/cmd/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	grpcadapter "task-tracker/gateway/internal/adapter/grpc"
+	httpadapter "task-tracker/gateway/internal/adapter/http"
+	"task-tracker/gateway/internal/config"
+)
+
+func main() {
+	conf := config.NewConfig()
+
+	userClient, err := grpcadapter.NewUserClient(conf.UserServiceAddr)
+	if err != nil {
+		log.Fatalf("failed to connect to user service: %v", err)
+	}
+
+	taskClient, err := grpcadapter.NewTaskClient(conf.TaskServiceAddr)
+	if err != nil {
+		log.Fatalf("failed to connect to task service: %v", err)
+	}
+
+	router := httpadapter.NewRouter(httpadapter.Handlers{
+		User: httpadapter.NewUserHandler(userClient),
+		Task: httpadapter.NewTaskHandler(taskClient),
+	})
+
+	log.Printf("gateway started, port = %s", conf.Port)
+	if err := http.ListenAndServe(conf.Port, router); err != nil {
+		log.Fatalf("server error: %v", err)
+	}
+}
