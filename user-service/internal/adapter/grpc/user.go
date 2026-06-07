edd040cc0bc66a3a9bdde8be06b054cc2b3be101
fanatioon <nazirov.f.f@gmail.com>
2026-06-07 18:56:09 +0300

    feat(user-service): add UserServer.GetUser grpc implementation

--- user-service/internal/adapter/grpc/user.go
+++ user-service/internal/adapter/grpc/user.go
@@ -6,6 +6,9 @@ import (
 	"task-tracker/user-service/internal/domain"
 	"task-tracker/user-service/internal/port/in"
 
+	"github.com/google/uuid"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 	"google.golang.org/protobuf/types/known/emptypb"
 	"google.golang.org/protobuf/types/known/timestamppb"
 )
@@ -36,8 +39,24 @@ func (u UserServer) CreateUser(ctx context.Context, request *userpb.CreateUserRe
 }
 
 func (u UserServer) GetUser(ctx context.Context, request *userpb.GetUserRequest) (*userpb.GetUserResponse, error) {
-	//TODO implement me
-	panic("implement me")
+	id, err := uuid.Parse(request.Id)
+	if err != nil {
+		return nil, status.Errorf(codes.InvalidArgument, "invalid id: %v", id)
+	}
+
+	response, err := u.service.GetUser(ctx, id)
+	if err != nil {
+		return nil, err
+	}
+
+	createResponse := userpb.User{
+		Id:        response.ID.String(),
+		Name:      response.Name,
+		Email:     response.Email,
+		CreatedAt: timestamppb.New(response.CreatedAt),
+		UpdatedAt: timestamppb.New(response.UpdatedAt),
+	}
+	return &userpb.GetUserResponse{User: &createResponse}, nil
 }
 
 func (u UserServer) UpdateUser(ctx context.Context, request *userpb.UpdateUserRequest) (*userpb.UpdateUserResponse, error) {
