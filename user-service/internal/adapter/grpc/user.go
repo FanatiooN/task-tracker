dfdfa3c2d742de704d1bb67b2a8b4ae146330055
fanatioon <nazirov.f.f@gmail.com>
2026-06-07 18:52:17 +0300

    feat(user-service): add UserServer.CreateUser grpc implementation

--- user-service/internal/adapter/grpc/user.go
+++ user-service/internal/adapter/grpc/user.go
@@ -3,9 +3,11 @@ package grpc
 import (
 	"context"
 	userpb "task-tracker/gen/proto/user"
+	"task-tracker/user-service/internal/domain"
 	"task-tracker/user-service/internal/port/in"
 
 	"google.golang.org/protobuf/types/known/emptypb"
+	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
 type UserServer struct {
@@ -13,8 +15,24 @@ type UserServer struct {
 }
 
 func (u UserServer) CreateUser(ctx context.Context, request *userpb.CreateUserRequest) (*userpb.CreateUserResponse, error) {
-	//TODO implement me
-	panic("implement me")
+	user := domain.User{
+		Name:  request.Name,
+		Email: request.Email,
+	}
+
+	response, err := u.service.CreateUser(ctx, user)
+	if err != nil {
+		return nil, err
+	}
+
+	createResponse := userpb.User{
+		Id:        response.ID.String(),
+		Name:      response.Name,
+		Email:     response.Email,
+		CreatedAt: timestamppb.New(response.CreatedAt),
+		UpdatedAt: timestamppb.New(response.UpdatedAt),
+	}
+	return &userpb.CreateUserResponse{User: &createResponse}, nil
 }
 
 func (u UserServer) GetUser(ctx context.Context, request *userpb.GetUserRequest) (*userpb.GetUserResponse, error) {
