b5f558fa3c9fd7c77c43efff9996336fef82a4a7
fanatioon <nazirov.f.f@gmail.com>
2026-06-07 18:20:26 +0300

    feat(user-service): add UserService.CreateUser implementation

--- user-service/internal/service/user.go
+++ user-service/internal/service/user.go
@@ -2,6 +2,8 @@ package service
 
 import (
 	"context"
+	"errors"
+	"strings"
 	"task-tracker/user-service/internal/domain"
 	"task-tracker/user-service/internal/port/out"
 
@@ -17,8 +19,31 @@ func NewUserService(repository out.UserRepository) *UserService {
 }
 
 func (u UserService) CreateUser(ctx context.Context, user domain.User) (domain.User, error) {
-	//TODO implement me
-	panic("implement me")
+	user.Name = strings.TrimSpace(user.Name)
+	if user.Name == "" {
+		return domain.User{}, errors.New("invalid name")
+	}
+
+	var email string
+
+	if user.Email != nil {
+		email = strings.TrimSpace(*user.Email)
+		if !validEmail.MatchString(email) {
+			return domain.User{}, errors.New("invalid email")
+		}
+
+		user.Email = &email
+	}
+
+	response, err := u.repository.Save(ctx, domain.User{
+		Name:  user.Name,
+		Email: user.Email,
+	})
+	if err != nil {
+		return domain.User{}, err
+	}
+
+	return response, nil
 }
 
 func (u UserService) GetUser(ctx context.Context, id uuid.UUID) (domain.User, error) {
