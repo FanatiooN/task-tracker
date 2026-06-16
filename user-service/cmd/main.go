4abe9652296e087c3b10df9c92c3055dc06efd40
fanatioon <nazirov.f.f@gmail.com>
2026-06-17 01:54:29 +0300

    feat(user-service): add graceful shutdown

--- user-service/cmd/main.go
+++ user-service/cmd/main.go
@@ -4,6 +4,9 @@ import (
 	"context"
 	"log"
 	"net"
+	"os"
+	"os/signal"
+	"syscall"
 	"task-tracker/gen/proto/user"
 	pkgdb "task-tracker/pkg/db"
 	grpcadapter "task-tracker/user-service/internal/adapter/grpc"
@@ -25,6 +28,7 @@ func main() {
 	}
 
 	defer pool.Close()
+
 	queries := db.New(pool)
 
 	repo := postgres.NewUserRepository(queries)
@@ -40,7 +44,23 @@ func main() {
 		log.Fatal(err)
 	}
 
-	if err := grpcServer.Serve(lis); err != nil {
+	errChan := make(chan error, 1)
+
+	go func(errChan chan<- error) {
+		err := grpcServer.Serve(lis)
+		if err != nil {
+			errChan <- err
+		}
+	}(errChan)
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGTERM, syscall.SIGINT)
+
+	select {
+	case <-sigChan:
+		grpcServer.GracefulStop()
+		return
+	case err := <-errChan:
 		log.Fatal(err)
 	}
 }
