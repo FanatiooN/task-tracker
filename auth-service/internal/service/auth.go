f21b1f172d97e2b41ac978f7c05803ab5f10ad50
fanatioon <nazirov.f.f@gmail.com>
2026-06-20 22:17:34 +0300

    feat(auth-service): add AuthService.LoginByEmail

--- auth-service/internal/service/auth.go
+++ auth-service/internal/service/auth.go
@@ -2,11 +2,15 @@ package service
 
 import (
 	"context"
+	"crypto/sha256"
+	"encoding/hex"
 	"task-tracker/auth-service/internal/domain"
 	"task-tracker/auth-service/internal/port/out"
 	"time"
 
+	"github.com/golang-jwt/jwt/v5"
 	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type AuthService struct {
@@ -15,6 +19,12 @@ type AuthService struct {
 	jwtSecret   string
 	accessTTL   time.Duration
 	refreshTTL  time.Duration
+}
+
+type AccessClaims struct {
+	UserID uuid.UUID
+	Email  string
+	jwt.RegisteredClaims
 }
 
 func NewAuthService(credentials out.CredentialRepository, tokens out.TokenRepository, jwtSecret string, accessTTL time.Duration, refreshTTL time.Duration) *AuthService {
@@ -28,8 +38,62 @@ func NewAuthService(credentials out.CredentialRepository, tokens out.TokenReposi
 }
 
 func (a AuthService) LoginByEmail(ctx context.Context, email, password string) (domain.Tokens, error) {
-	//TODO implement me
-	panic("implement me")
+	creds, err := a.credentials.FindByEmail(ctx, email)
+	if err != nil {
+		return domain.Tokens{}, err
+	}
+
+	err = bcrypt.CompareHashAndPassword([]byte(creds.PasswordHash), []byte(password))
+	if err != nil {
+		return domain.Tokens{}, err
+	}
+
+	tokens, err := a.generateAccessToken(ctx, creds.UserID, creds.Email)
+	if err != nil {
+		return domain.Tokens{}, err
+	}
+
+	return tokens, nil
+}
+
+func (a AuthService) generateAccessToken(ctx context.Context, userID uuid.UUID, email string) (domain.Tokens, error) {
+	now := time.Now()
+
+	expiresAt := jwt.NewNumericDate(now.Add(a.accessTTL))
+	claims := AccessClaims{
+		UserID: userID,
+		Email:  email,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: expiresAt,
+			IssuedAt:  jwt.NewNumericDate(now),
+		},
+	}
+
+	accessToken, err := jwt.NewWithClaims(jwt.SigningMethodHS256, claims).SignedString([]byte(a.jwtSecret))
+	if err != nil {
+		return domain.Tokens{}, err
+	}
+
+	refreshToken := uuid.NewString()
+	hash := sha256.Sum256([]byte(refreshToken))
+	tokenHash := hex.EncodeToString(hash[:])
+
+	err = a.tokens.Save(ctx, domain.RefreshToken{
+		ID:        uuid.New(),
+		UserID:    userID,
+		TokenHash: tokenHash,
+		ExpiresAt: expiresAt.Time,
+		IsRevoked: false,
+	})
+
+	if err != nil {
+		return domain.Tokens{}, err
+	}
+
+	return domain.Tokens{
+		AccessToken:  accessToken,
+		RefreshToken: refreshToken,
+	}, nil
 }
 
 func (a AuthService) RefreshToken(ctx context.Context, refreshToken string) (domain.Tokens, error) {
