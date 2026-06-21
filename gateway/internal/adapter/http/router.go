5b7d315d384c25f6ef1c51903443ce2e3d92142e
fanatioon <nazirov.f.f@gmail.com>
2026-06-21 16:30:47 +0300

    feat(gateway): add routes for auth-service handlers

--- gateway/internal/adapter/http/router.go
+++ gateway/internal/adapter/http/router.go
@@ -5,6 +5,7 @@ import "net/http"
 type Handlers struct {
 	User *UserHandler
 	Task *TaskHandler
+	Auth *AuthHandler
 }
 
 func NewRouter(h Handlers) http.Handler {
@@ -14,6 +15,10 @@ func NewRouter(h Handlers) http.Handler {
 	mux.HandleFunc("GET /users/{id}", h.User.GetUser)
 	mux.HandleFunc("PUT /users/{id}", h.User.UpdateUser)
 	mux.HandleFunc("DELETE /users/{id}", h.User.DeleteUser)
+
+	mux.HandleFunc("POST /login", h.Auth.Login)
+	mux.HandleFunc("POST /refresh", h.Auth.Refresh)
+	mux.HandleFunc("POST /logout", h.Auth.Logout)
 
 	mux.HandleFunc("POST /tasks", h.Task.CreateTask)
 	mux.HandleFunc("GET /tasks/{id}", h.Task.GetTask)
