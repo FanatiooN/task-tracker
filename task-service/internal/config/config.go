d6a508a1f6dc8bb5b03b143a512ba271bdbfc0a5
fanatioon <nazirov.f.f@gmail.com>
2026-06-25 16:19:57 +0300

    feat(task-service): add config

---
+++ task-service/internal/config/config.go
@@ -0,0 +1,45 @@
+package config
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	GRPCPort string
+
+	DBHost     string
+	DBPort     string
+	DBUser     string
+	DBPassword string
+	DBName     string
+}
+
+func NewConfig() *Config {
+	_ = godotenv.Load("task-service/.env")
+
+	return &Config{
+		GRPCPort: getEnv("GRPC_PORT", ":50051"),
+
+		DBHost:     getEnv("DB_HOST", "localhost"),
+		DBPort:     getEnv("DB_PORT", "5432"),
+		DBUser:     getEnv("DB_USER", "postgres"),
+		DBPassword: getEnv("DB_PASSWORD", "postgres"),
+		DBName:     getEnv("DB_NAME", "postgres"),
+	}
+}
+
+func (c *Config) DatabaseURL() string {
+	return fmt.Sprintf("postgres://%s:%s@%s:%s/%s",
+		c.DBUser, c.DBPassword, c.DBHost, c.DBPort, c.DBName)
+}
+
+func getEnv(key string, defaultVal string) string {
+	if value, exists := os.LookupEnv(key); exists {
+		return value
+	}
+
+	return defaultVal
+}
