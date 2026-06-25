bac89c064da594e7bf3200acacf79279a5f8037b
fanatioon <nazirov.f.f@gmail.com>
2026-06-25 06:57:05 +0300

    feat(task-service): add TaskRepository outbound port

---
+++ task-service/internal/port/out/task.go
@@ -0,0 +1,16 @@
+package out
+
+import (
+	"context"
+	"task-tracker/task-service/internal/domain"
+
+	"github.com/google/uuid"
+)
+
+type TaskRepository interface {
+	Save(ctx context.Context, task domain.Task) (domain.Task, error)
+	FindByID(ctx context.Context, id uuid.UUID) (domain.Task, error)
+	List(ctx context.Context, params domain.ListTasksParams) ([]domain.Task, error)
+	Update(ctx context.Context, task domain.Task) (domain.Task, error)
+	Delete(ctx context.Context, id []uuid.UUID) error
+}
