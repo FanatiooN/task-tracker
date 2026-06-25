0f74fe0fe6fc704962249e1e229ac37c30a55036
fanatioon <nazirov.f.f@gmail.com>
2026-06-25 06:56:52 +0300

    feat(task-service): add TaskService inbound port

---
+++ task-service/internal/port/in/task.go
@@ -0,0 +1,16 @@
+package in
+
+import (
+	"context"
+	"task-tracker/task-service/internal/domain"
+
+	"github.com/google/uuid"
+)
+
+type TaskService interface {
+	CreateTask(ctx context.Context, task domain.Task) (domain.Task, error)
+	GetTask(ctx context.Context, id uuid.UUID) (domain.Task, error)
+	ListTasks(ctx context.Context, params domain.ListTasksParams) ([]domain.Task, string, error)
+	UpdateTask(ctx context.Context, id uuid.UUID, task domain.Task) (domain.Task, error)
+	DeleteTasks(ctx context.Context, id []uuid.UUID) error
+}
