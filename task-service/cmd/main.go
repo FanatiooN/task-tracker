99729be74b074d07cd9b8e2b60da8717f69337e3
fanatioon <nazirov.f.f@gmail.com>
2026-06-25 16:41:44 +0300

    feat(task-service): add main

---
+++ task-service/cmd/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
+	"task-tracker/gen/proto/task"
+	pkgdb "task-tracker/pkg/db"
+	grpcadapter "task-tracker/task-service/internal/adapter/grpc"
+	"task-tracker/task-service/internal/adapter/postgres"
+	"task-tracker/task-service/internal/config"
+	"task-tracker/task-service/internal/db"
+	"task-tracker/task-service/internal/service"
+
+	"google.golang.org/grpc"
+)
+
+func main() {
+	conf := config.NewConfig()
+	ctx := context.Background()
+
+	pool, err := pkgdb.NewPool(ctx, conf.DatabaseURL())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer pool.Close()
+
+	queries := db.New(pool)
+
+	repo := postgres.NewTaskRepository(queries)
+	taskService := service.NewTaskService(repo)
+	server := grpcadapter.NewTaskServer(taskService)
+
+	grpcServer := grpc.NewServer()
+
+	task.RegisterTaskServiceServer(grpcServer, server)
+
+	lis, err := net.Listen("tcp", conf.GRPCPort)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	errChan := make(chan error, 1)
+
+	go func(errChan chan<- error) {
+		err := grpcServer.Serve(lis)
+		if err != nil {
+			errChan <- err
+		}
+	}(errChan)
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGTERM, syscall.SIGINT)
+
+	select {
+	case <-sigChan:
+		grpcServer.GracefulStop()
+		return
+	case err := <-errChan:
+		log.Fatal(err)
+	}
+}
