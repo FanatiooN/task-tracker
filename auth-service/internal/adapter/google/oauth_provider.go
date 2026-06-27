814cee37256c38785b7f094e77ed957d46dcbfea
fanatioon <nazirov.f.f@gmail.com>
2026-06-27 03:58:37 +0300

    feat(auth-service): add google adapter for OAuthProvider

---
+++ auth-service/internal/adapter/google/oauth_provider.go
@@ -0,0 +1,62 @@
+package google
+
+import (
+	"context"
+	"encoding/json"
+	"task-tracker/auth-service/internal/domain"
+
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+)
+
+type OAuthProvider struct {
+	clientID     string
+	clientSecret string
+}
+
+func NewOAuthProvider(clientID, clientSecret string) *OAuthProvider {
+	return &OAuthProvider{
+		clientID:     clientID,
+		clientSecret: clientSecret,
+	}
+}
+
+func (O OAuthProvider) GetUserInfo(ctx context.Context, provider, code, redirectURI string) (domain.OAuthUserInfo, error) {
+	oAuthConf := oauth2.Config{
+		ClientID:     O.clientID,
+		ClientSecret: O.clientSecret,
+		Endpoint:     google.Endpoint,
+		RedirectURL:  redirectURI,
+		Scopes:       []string{"openid", "email", "profile"},
+	}
+
+	token, err := oAuthConf.Exchange(ctx, code)
+	if err != nil {
+		return domain.OAuthUserInfo{}, err
+	}
+
+	client := oAuthConf.Client(ctx, token)
+	resp, err := client.Get("https://www.googleapis.com/oauth2/v2/userinfo")
+	if err != nil {
+		return domain.OAuthUserInfo{}, err
+	}
+	defer resp.Body.Close()
+
+	var req struct {
+		ID    string  `json:"id"`
+		Name  string  `json:"name"`
+		Email *string `json:"email"`
+	}
+
+	if decodeErr := json.NewDecoder(resp.Body).Decode(&req); decodeErr != nil {
+		return domain.OAuthUserInfo{}, decodeErr
+	}
+
+	info := domain.OAuthUserInfo{
+		ID:    req.ID,
+		Name:  req.Name,
+		Email: req.Email,
+	}
+
+	return info, nil
+}
