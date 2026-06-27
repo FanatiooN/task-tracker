9ff159372f38717dee034cb5beb4bf0f9779e3a9
fanatioon <nazirov.f.f@gmail.com>
2026-06-27 05:41:21 +0300

    feat(gateway): add OAuth config

--- gateway/internal/config/config.go
+++ gateway/internal/config/config.go
@@ -7,16 +7,28 @@ import (
 )
 
 type Config struct {
+	OAuth OAuthConfig
+
 	Port            string
 	UserServiceAddr string
 	TaskServiceAddr string
 	AuthServiceAddr string
 }
 
+type OAuthConfig struct {
+	GoogleClientID    string
+	GoogleRedirectURI string
+}
+
 func NewConfig() *Config {
 	_ = godotenv.Load("gateway/.env")
 
 	return &Config{
+		OAuth: OAuthConfig{
+			GoogleClientID:    getEnv("OAUTH_GOOGLE_CLIENT_ID", "id"),
+			GoogleRedirectURI: getEnv("OAUTH_GOOGLE_REDIRECT_URI", "http://localhost:3000/login/google"),
+		},
+
 		Port:            getEnv("GATEWAY_PORT", ":8080"),
 		UserServiceAddr: getEnv("USER_SERVICE_ADDR", "localhost:50051"),
 		TaskServiceAddr: getEnv("TASK_SERVICE_ADDR", "localhost:50052"),
