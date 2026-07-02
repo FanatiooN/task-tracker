989ae3ff51e7d7eb57072cd5a4fb488c56b7c670
fanatioon <nazirov.f.f@gmail.com>
2026-07-02 06:56:25 +0300

    fix: change http method GET to POST in login/google

--- gateway/internal/adapter/http/router.go
+++ gateway/internal/adapter/http/router.go
@@ -21,9 +21,9 @@ func NewRouter(h Handlers, authClient authpb.AuthServiceClient) http.Handler {
 	mux.HandleFunc("POST /register", h.Auth.Register)
 	mux.HandleFunc("POST /refresh", h.Auth.Refresh)
 
-	mux.HandleFunc("GET /login/google", h.OAuth.LoginWithGoogle)
-	mux.HandleFunc("GET /login/google/callback", h.OAuth.LoginCallbackWithGoogle)
 	mux.HandleFunc("POST /login/telegram", h.OAuth.LoginWithTelegram)
+	mux.HandleFunc("POST /login/google", h.OAuth.LoginWithGoogle)
+	mux.HandleFunc("GET /login/google/callback", h.OAuth.LoginCallbackWithGoogle)
 
 	mux.Handle("GET /users/{id}", auth(http.HandlerFunc(h.User.GetUser)))
 	mux.Handle("PUT /users/{id}", auth(http.HandlerFunc(h.User.UpdateUser)))
